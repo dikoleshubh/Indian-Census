7164f97f008e9f55dc52c9abfcc6aaf9521d95a3
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-23 08:26:20 +0530

    [Arti Dandge] Add : Display Welcome Message on Console

---
+++ IndianStateCensusAndCodeAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusAndCodeAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*******Welcome to Indian State Census Analyser*******");
+        }
+    }
+}
