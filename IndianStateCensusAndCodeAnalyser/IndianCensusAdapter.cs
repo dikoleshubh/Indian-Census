11ea408cf4af55fa8e709c81c316e8eb4cc3bdde
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-24 10:08:42 +0530

    [Arti Dandge] Add : Indian State Code CSV file correction

--- IndianStateCensusAndCodeAnalyser/IndianCensusAdapter.cs
+++ IndianStateCensusAndCodeAnalyser/IndianCensusAdapter.cs
@@ -23,7 +23,7 @@ namespace IndianStateCensusAndCodeAnalyser
                 string[] column = data.Split(",");
                 if (csvfilePath.Contains("IndianStateCode.csv"))
                 {
-                    dataMap.Add(column[1], new CensusDTO(new StateCodeDAO(column[0], column[1], column[2], column[3])));
+                    dataMap.Add(column[0], new CensusDTO(new StateCodeDAO(column[0], column[1], column[2], column[3])));
                 }
                 if (csvfilePath.Contains("IndianStateCensusData.csv"))
                 {
