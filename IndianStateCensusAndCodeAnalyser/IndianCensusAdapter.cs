a561b59fcb6edd67182969a366527b072077d823
dikoleshubh <dikole.shubh@gmail.com>
2021-06-04 13:43:04 +0530

    Ability for the analyser to load the Indian States Code Information from a csv file

--- IndianStateCensusAndCodeAnalyser/IndianCensusAdapter.cs
+++ IndianStateCensusAndCodeAnalyser/IndianCensusAdapter.cs
@@ -21,6 +21,10 @@ namespace IndianStateCensusAndCodeAnalyser
                     throw new CensusAnalyserException("File Contains wrong Delimeter", CensusAnalyserException.ExceptionType.INCORRECT_DELIMITER);
                 }
                 string[] column = data.Split(",");
+                if (csvfilePath.Contains("IndianStateCode.csv"))
+                {
+                    dataMap.Add(column[0], new CensusDTO(new StateCodeDAO(column[0], column[1], column[2], column[3])));
+                }
                 if (csvfilePath.Contains("IndianStateCensusData.csv"))
                 {
                     dataMap.Add(column[0], new CensusDTO(new CensusDataDAO(column[0], column[1], column[2], column[3])));
